258ccebc72546d98d15137652d72d15904b85d34
The-Graze <reece2004f@gmail.com>
2023-05-26 20:41:00 +0100

    Add project files.

---
+++ BUGCAM!!/Plugin.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+using Utilla;
+
+namespace BUGCAM__
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        bool bugcam;
+        GameObject cAM;
+        GameObject Bug;
+        void Start()
+        {
+            cAM = GameObject.Find("Shoulder Camera");
+            Bug = GameObject.Find("Floating Bug Holdable");
+        }
+        void OnGUI()
+        {
+            bugcam = GUI.Toggle(new Rect(0, 120, 100, 100), bugcam, "BUGCAM!");
+        }
+        void Update()
+        {
+            if (Bug == null) Start();
+            if (bugcam)
+            {
+                cAM.transform.GetChild(0).gameObject.SetActive(false);
+                cAM.transform.position = Bug.transform.position;
+                cAM.transform.rotation = Bug.transform.rotation;
+            }
+            else 
+            {
+                cAM.transform.GetChild(0).gameObject.SetActive(true);
+            }
+        }
+    }
+}
